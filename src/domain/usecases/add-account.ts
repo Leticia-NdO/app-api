fbf91da345a6cc56c9f39db4df35f12c63825395
Leticia-NdO <3972656@alunouninter.com>
2022-11-16 15:47:29 -0300

    feat: ensure DbAddAccount calls Encrypter with correct password

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -7,5 +7,5 @@ export interface AddAccountModel {
 }
 
 export interface AddAccount {
-  add: (accout: AddAccountModel) => Promise<AccountModel>
+  add: (account: AddAccountModel) => Promise<AccountModel>
 }
