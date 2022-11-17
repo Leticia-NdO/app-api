b0087bc3719639cee56737b29a3e6c5c24022312
Leticia-NdO <3972656@alunouninter.com>
2022-11-17 09:16:52 -0300

    test: ensure bcrypt returns a hash on success

--- src/infra/cryptograph/bcrypt-adapter.spec.ts
+++ src/infra/cryptograph/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdapter } from './bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hash'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   it('Should call Bcrypt with correct values', async () => {
     const salt = 12
@@ -13,5 +19,12 @@ describe('Bcrypt Adapter', () => {
     expect(bcrypt.hash).toBeCalledWith(password, salt)
   })
 
-  
+  it('Should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const password = 'valid_password'
+    const hash = await sut.encrypt(password)
+
+    expect(hash).toBe('hash')
+  })
 })
