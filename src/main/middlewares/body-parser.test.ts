dcc1d756964f4a8e034c693c2c38824b22e31ea6
Leticia-NdO <3972656@alunouninter.com>
2022-11-28 17:21:47 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,8 +1,6 @@
 import request from 'supertest'
 import app from '../config/app'
 
-// aula 8 parte 
-
 describe('Body Parser Middleware', () => {
   it('Should parse body as json', async () => {
     app.post('/test_body_parser', (req, res) => {
