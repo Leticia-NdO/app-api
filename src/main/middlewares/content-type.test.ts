6978fd95de3deb7beb3807afef4372c87453b88e
Leticia-NdO <3972656@alunouninter.com>
2022-11-29 09:30:33 -0300

    refactor: group middlewares intefaces in a single file

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -1,6 +1,8 @@
 import app from '../config/app'
 import request from 'supertest'
 
+// [ ] Aula 8 parte 2 00:00
+
 describe('Content Type Middleware', () => {
   it('Should return default content-type as json', async () => {
     app.get('/test_content_type', (req, res) => {
