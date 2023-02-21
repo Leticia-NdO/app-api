ce4f10e6612ec91e491d658e84433369ec0e97dd
Leticia-NdO <3972656@alunouninter.com>
2023-02-21 17:33:18 -0300

    feat: ensure POST /surveys returns 204 on success

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
+import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
 import { makeLogControllerDecorator } from '../factories/decorators/log-controller-decorator-factory'
 
 export default (router: Router): void => {
-  router.post('/surveys', adaptRoute(makeLogControllerDecorator(makeSignUpController())))
+  router.post('/surveys', adaptRoute(makeLogControllerDecorator(makeAddSurveyController())))
 }
