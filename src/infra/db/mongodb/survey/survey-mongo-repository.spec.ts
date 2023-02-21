8923597af598ca18c9d50fa96c51efe1686e8335
Leticia-NdO <3972656@alunouninter.com>
2023-02-21 17:04:03 -0300

    test: ensure SurveyMongoRepository adds a survey on surveys collection on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,45 @@
+import { Collection } from 'mongodb'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+let surveyCollection: Collection
+
+describe('Account Mondo Repository', () => {
+  // Quando fazemos testes com bancos de dados é necessário conectar-se ao db antes dos testes e desconectar depois.
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({}) // se passarmos um objeto vazio todos os registros dessa collection vão ser deletados
+  })
+
+  const makeFakeAddSurveyModel = (): AddSurveyModel => ({
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }]
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  it('Should add a survey on surveys collection on add success', async () => {
+    const sut = makeSut()
+
+    const data = makeFakeAddSurveyModel()
+
+    await sut.add(data)
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+
+    expect(survey).toBeTruthy()
+  })
+})
