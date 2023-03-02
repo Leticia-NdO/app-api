472091b719d3305f3b32e9bd2a0d0e9637adda41
Leticia-NdO <3972656@alunouninter.com>
2023-03-02 17:13:35 -0300

    feat: ensure LoadSurveysController calls LoadSurveys

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,13 @@
+import { noContent } from '../../login/login/login-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-surveys-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) {
+
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return noContent()
+  }
+}
---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-protocols.ts
@@ -0,0 +1,4 @@
+export { Controller, HttpRequest, HttpResponse } from '../../../protocols'
+export { ok } from '../../login/login/login-controller-protocols'
+export * from '../../../../domain/usecases/load-surveys'
+export * from '../../../../domain/models/survey'
