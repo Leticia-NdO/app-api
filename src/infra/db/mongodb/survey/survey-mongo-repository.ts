ec43ff543965bc92ceb1851e530f7767487bde8d
Leticia-NdO <3972656@alunouninter.com>
2023-04-24 18:28:49 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,20 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/db-load-surveys-repository'
 import { AddSurveyModel, AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { SurveyModel } from '../../../../domain/models/survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (data: AddSurveyModel): Promise<void> {
-    const accountCollection = await MongoHelper.getCollection('surveys')
+    const surveysCollection = await MongoHelper.getCollection('surveys')
 
-    await accountCollection.insertOne(data)
+    await surveysCollection.insertOne(data)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveysCollection = await MongoHelper.getCollection('surveys')
+
+    const surveys = await surveysCollection.find().toArray() as unknown as SurveyModel[]
+
+    return surveys
   }
 }
